ed2fcb875297fc30a492cb4e84a06d61f56f9696
Your Name <you@example.com>
2023-02-03 22:47:47 +0700

    modul_4

---
+++ thuc_hanh_quan_ly_KS_JPA/src/main/java/com/codegym/controller/CustomerController.java
@@ -0,0 +1,107 @@
+package com.codegym.controller;
+
+
+import com.codegym.model.Customer;
+import com.codegym.model.Province;
+import com.codegym.service.ICustomerService;
+import com.codegym.service.IProvinceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class CustomerController {
+    @Autowired
+    private ICustomerService customerService;
+
+    @Autowired
+    private IProvinceService provinceService;
+
+    @ModelAttribute("provinces")
+    public Iterable<Province> provinces(){
+        return provinceService.findAll();
+    }
+
+    @GetMapping("/create-customer")
+    public ModelAndView showCreate(){
+        ModelAndView modelAndView = new ModelAndView("/create");
+        modelAndView.addObject("customer", new Customer());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-customer")
+    public ModelAndView saveCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/create");
+        modelAndView.addObject("customer", new Customer());
+        modelAndView.addObject("message", "New customer created successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/customers")
+    public ModelAndView listCustomer(@RequestParam(name = "page", required = false, defaultValue = "0") Integer page,
+                                     @RequestParam(name = "size", required = false, defaultValue = "5") Integer size,
+                                     @RequestParam(name = "sort", required = false, defaultValue = "ASC") String sort,
+                                     @RequestParam("search") Optional<String> search, Pageable pageable){
+        Page<Customer> customers ;
+        pageable = PageRequest.of(page, size);
+        if (search.isPresent()){
+            customers = customerService.findAllByFirstNameContaining(search.get(),pageable);
+        }else {
+            customers = customerService.findAll(pageable);
+        }
+        ModelAndView modelAndView = new ModelAndView("/list");
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+
+    @GetMapping("/edit-customer/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id){
+        Optional<Customer> customer = customerService.findById(id);
+        if ( customer != null){
+            ModelAndView modelAndView = new ModelAndView("/edit");
+            modelAndView.addObject("customer", customer.get());
+            return modelAndView;
+        }else {
+            ModelAndView modelAndView = new ModelAndView("/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/edit-customer")
+    public ModelAndView updateCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/edit");
+        modelAndView.addObject("customer", customer);
+        modelAndView.addObject("message", "Customer updated successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete-customer/{id}")
+    public ModelAndView showDeleteForm(@PathVariable Long id){
+        Optional<Customer> customer = customerService.findById(id);
+        if(customer != null){
+            ModelAndView modelAndView = new ModelAndView("/delete");
+            modelAndView.addObject("customer", customer.get());
+            return modelAndView;
+        }else {
+            ModelAndView modelAndView = new ModelAndView("/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/delete-customer")
+    public String deleteCustomer(@ModelAttribute("customer") Customer customer){
+        customerService.remove(customer.getId());
+        return "redirect:customers";
+    }
+}
---
+++ thuc_hanh_quan_ly_KS_JPA/src/main/java/com/codegym/service/ICustomerService.java
@@ -0,0 +1,14 @@
+package com.codegym.service;
+
+import com.codegym.model.Customer;
+import com.codegym.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+    Page<Customer> findAll(Pageable pageable);
+
+    Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable);
+
+
+}
